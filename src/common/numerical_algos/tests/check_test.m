1bf6a72a70e0bed91ba3a7e79793ad07ab178146
Oleg Ryabkov <oleg.ryabkov.87@gmail.com>
2019-10-13 15:37:52 +0300

    removed solvers tests

--- src/common/numerical_algos/tests/check_test.m
+++
@@ -1,9 +0,0 @@
-function check_test(A_fn,rhs_fn,res_fn)
-    A = read_mat(A_fn);
-    rhs = read_vec(rhs_fn);
-    res = read_vec(res_fn);
-    res_correct = A\rhs;
-    fprintf('delta_norm = %f\n', norm(res-res_correct,Inf));
-    write_vec('res_correct.dat', res_correct);
-    write_vec('delta_res.dat', res-res_correct);
-end
--- src/common/numerical_algos/tests/write_vec.m
+++
@@ -1,6 +0,0 @@
-function write_vec(fn,vec)
-    fid = fopen(fn,'wt');
-    fprintf(fid, '%d %d\n', size(vec,1), 1);
-    fclose(fid);
-    dlmwrite(fn,vec, '-append', 'delimiter', '\t', 'precision','%0.8e')
-end
